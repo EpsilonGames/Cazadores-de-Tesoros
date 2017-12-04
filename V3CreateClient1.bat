9049406e4afac5e08d717586f62fab0e0f9c42b6
Everardo Hernandez <everardo.hernandezdz@gmail.com>
2017-12-03 23:39:10 -0600

    Cambio de gitignore, se arregla bug en el mapa y se crean archivos bat para el servidor dedicado

---
+++ V3CreateClient1.bat
@@ -0,0 +1,2 @@
+TIMEOUT /T 6
+ "C:\Program Files\Epic Games\UE_4.16\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\v3_RobertoVillegas.uproject" 192.168.1.68 -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log -nosteam
---
+++ V3CreateDedicatedServer.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_4.16\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\v3_RobertoVillegas.uproject" V3_ProyectoFinal -server -log -nosteam
